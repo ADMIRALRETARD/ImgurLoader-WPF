20742cd47ed3fc028f7335c4c813af2a7cdbcc96
Kirill Kalyonov <buoiiiok@yahoo.com>
2018-09-14 18:39:36 +0300

    added basic functional

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Imgur.API.Authentication.Impl;
+using Imgur.API.Endpoints.Impl;
+using Imgur.API.Models;
+using Microsoft.Win32;
+using System.IO;
 
 namespace WPF_ImgurLoader
 {
@@ -20,9 +25,46 @@ namespace WPF_ImgurLoader
     /// </summary>
     public partial class MainWindow : Window
     {
+        OpenFileDialog _openDialog = new OpenFileDialog();
         public MainWindow()
         {
+
             InitializeComponent();
+        }
+
+        private void pathFilebtn_Click(object sender, RoutedEventArgs e)
+        {
+            _openDialog.Filter = "Image Files(*.BMP; *.JPG; *.GIF;*.PNG)| *.BMP; *.JPG; *.GIF;*.PNG | All files(*.*) | *.*";
+            if (_openDialog.ShowDialog() == false)
+                return;
+            string filename = _openDialog.FileName;
+            pathFiletb.Text = filename;
+        }
+
+        private void fileUploadbtn_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new ImgurClient("b67ea901a7c3d32", "f38dd43c6aff1e4062c4abdc724e6a11271af6c1");
+            var endpoint = new ImageEndpoint(client);
+            IImage image;
+            string path = pathFiletb.Text;
+            try
+            {
+
+                if (File.Exists(path))
+                {
+
+                    using (var fs = new FileStream(path, FileMode.Open))
+                    {
+                        image = endpoint.UploadImageStreamAsync(fs).GetAwaiter().GetResult();
+                    }
+                    MessageBox.Show("image uploaded");
+                    get_Url.Text = image.Link;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Произошла ошибка при загрузке изображения");
+            }
         }
     }
 }
